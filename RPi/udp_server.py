ba1b03f58065613f187d54a996f1b88081b045c0
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2021-12-06 15:12:49 +0100

    Added detection for different devices and ASNs. Updated time variables

--- RPi/udp_server.py
+++ RPi/udp_server.py
@@ -1,25 +1,27 @@
-# Replace RPi libraries by fake ones
 import sys
-try:
+try: #If run on MacOS
 	import fake_rpi
 	sys.modules['RPi'] = fake_rpi.RPi # Fake RPi
 	sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
 	import RPi.GPIO as GPIO
-except ModuleNotFoundError:
+except ModuleNotFoundError: #If run on RPi
 	import RPi.GPIO as GPIO
 import datetime
 import socket
 import re
-from datetime import datetime
+from datetime import date, datetime
 import signal
   
-UDP_IP = "::" # = 0.0.0.0 u IPv4
+UDP_IP = "::" # = 0.0.0.0 in IPv4
 UDP_PORT = 5678
 SENSOR_IP = "fd00::212:4b00:f8e:2584"
 BORDER_ROUTER_IP = "fd00::212:4b00:f19:b001"
-SENSOR_UTC = datetime.utcnow()
-BORDER_ROUTER_UTC = datetime.utcnow()
-
+TIME_BORDER_ROUTER_INT = datetime.utcnow()
+TIME_BORDER_ROUTER_REC = datetime.utcnow()
+TIME_SENSOR_INT = datetime.utcnow()
+TIME_SENSOR_REC = datetime.utcnow()
+ASN_BORDER_ROUTER = 0
+ASN_SENSOR = 0
 
 SENSOR_GPIO = 5 
 BORDER_ROUTER_GPIO = 6
@@ -29,10 +31,10 @@ def signal_handler(sig, frame):
 	sys.exit(0)
 
 def sensor_sent_callback(channel):
-	print("sensor pin pulled low")
+	print("sensor pin pulled up")
 
 def border_router_sent_callback(channel):
-	print("border router pin pulled low")
+	print("border router pin pulled up")
 
 def main():
 	sock = socket.socket(socket.AF_INET6, # Internet
@@ -46,19 +48,18 @@ def main():
 	GPIO.add_event_detect(BORDER_ROUTER_GPIO, GPIO.RISING, callback=border_router_sent_callback)
 
 	signal.signal(signal.SIGINT, signal_handler)
-	#signal.pause()
 	
-
 	while True:
 		data, addr = sock.recvfrom(1024) # Receive data from socket, buffersize 1024 bytes
 		data_split = re.split(': |, ', data.decode('utf-8')) # Interpret data
-		asn = (int(data_split[1]) << 32) + int(data_split[3]) # Bitshift msb 4 bytes to the left and add with lsb to get full ASN
 		if(addr[0] == SENSOR_IP):
-			SENSOR_UTC = datetime.utcnow()
-			print("Sensor ASN:", asn, "Timestamp:", SENSOR_UTC)
+			TIME_SENSOR_REC = datetime.utcnow()
+			ASN_SENSOR = (int(data_split[1]) << 32) + int(data_split[3]) # Bitshift msb 4 bytes to the left and add with lsb to get full ASN
+			print("Sensor ASN:", ASN_SENSOR, "Timestamp:", TIME_SENSOR_REC)
 		elif(addr[0] == BORDER_ROUTER_IP):
-			BORDER_ROUTER_UTC = datetime.utcnow()
-			print("Border Router ASN:", asn, "Timestamp:", BORDER_ROUTER_UTC)
+			TIME_BORDER_ROUTER_REC = datetime.utcnow()
+			ASN_BORDER_ROUTER = (int(data_split[1]) << 32) + int(data_split[3]) # Bitshift msb 4 bytes to the left and add with lsb to get full ASN
+			print("Border Router ASN:", ASN_BORDER_ROUTER, "Timestamp:", TIME_BORDER_ROUTER_REC)
 
 
 if __name__ == "__main__":
