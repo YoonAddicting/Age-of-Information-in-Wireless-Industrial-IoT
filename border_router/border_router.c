d60dddd09acf309d449d17c99c27e80c32ef4620
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2021-12-07 21:20:29 +0100

    Cleanup so codebase for sensor and border_router is as similar as possible. Save ASN to variables just after signifying that it have been measured, and send the same ones over udp.

--- border_router/border_router.c
+++ border_router/border_router.c
@@ -1,24 +1,34 @@
 #include "contiki.h"
+// For routing
 #include "net/routing/routing.h" // For setting border router to root
+// For MAC layer
 #include "net/netstack.h" // Start TSCH
-#include "net/ipv6/simple-udp.h" // To send UDP packages to RPi
+#include "os/net/mac/tsch/tsch.h" // Access ASN
+// For sending UDP packets over IPv6
+#include "net/ipv6/simple-udp.h"
+#include "os/net/ipv6/uiplib.h"
 
-#include "dev/gpio-hal.h" // For pulling GPIO pins low
+// For GPIO
+#include "dev/gpio-hal.h"
 #include "arch/platform/simplelink/cc13xx-cc26xx/launchpad/cc2650/Board.h"
 
-#include "sys/log.h" // For simple logging
+// Project general
+#include "project-conf.h"
+
+// Setup for logging to serial
+#include "sys/log.h"
 #define LOG_MODULE "AoI Border Router"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  0
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
+#define SEND_INTERVAL		  (20 * CLOCK_SECOND)
+
+static struct simple_udp_connection udp_conn; // UDP Connection
+
 gpio_hal_pin_t out_pin1 = Board_PIN_RLED;
 gpio_hal_pin_t out_pin2 = Board_PIN_GLED;
-
-//extern gpio_hal_pin_t out_pin1, out_pin2, out_pin3;
-//extern gpio_hal_pin_t btn_pin;
 
 #if !GPIO_HAL_PORT_PIN_NUMBERING
 extern gpio_hal_port_t out_port1, out_port2_3;
@@ -29,8 +39,7 @@ extern gpio_hal_port_t btn_port;
 #define btn_port    GPIO_HAL_NULL_PORT
 #endif
 
-static struct simple_udp_connection udp_conn;
-
+/*---------------------------------------------------------------------------*/
 PROCESS(aoi_border_router_process, "AoI Border Router");
 AUTOSTART_PROCESSES(&aoi_border_router_process);
 /*---------------------------------------------------------------------------*/
@@ -40,42 +49,51 @@ udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr
   LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
   LOG_INFO_6ADDR(sender_addr);
   LOG_INFO_("\n");
-  #if WITH_SERVER_REPLY
-  /* send back the same string to the client as an echo reply */
-  LOG_INFO("Sending response.\n");
-  simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
-  #endif /* WITH_SERVER_REPLY */
 }
-bool red = 1;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(aoi_border_router_process, ev, data) {
-  PROCESS_BEGIN();
+  static struct etimer periodic_timer; // Timer for sending data periodically
+  static char udp_buf[32]; // char[] buffer for sending data to RPi
+  static uip_ipaddr_t rpi_ip; // IP for the RPi
+  uiplib_ipaddrconv("fd00:0:0:0:0:0:0:1", &rpi_ip);
+  // Int's for ASN
+  static int ms1b;
+  static long ls4b;
 
+  PROCESS_BEGIN(); // Begin the process
+
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, udp_rx_callback);
+                    
   /* Initialize DAG root */
   NETSTACK_ROUTING.root_start();
   NETSTACK_MAC.on();
 
-  /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
-                      UDP_CLIENT_PORT, udp_rx_callback);
-
   LOG_INFO("AoI Border Router started\n");
 
-  static struct etimer et;
-  while(1){
-    etimer_set(&et, 5*CLOCK_SECOND);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-    if(!red){
-      gpio_hal_arch_write_pin(out_port1, out_pin1, 1);
-      gpio_hal_arch_write_pin(out_port2, out_pin2, 0);
-      red = 1;
-      LOG_INFO("Red LED is on\n");
-    } else {
-      gpio_hal_arch_write_pin(out_port1, out_pin1, 0);
-      gpio_hal_arch_write_pin(out_port2, out_pin2, 1);
-      red = 0;
-      LOG_INFO("Green LED is on\n");
-    } 
+  while(1) {
+    /* Set a timer for SEND_INTERVAL seconds*/
+    etimer_set(&periodic_timer, SEND_INTERVAL);
+    
+    /* Send ASN to RPi */
+    // Set pin high, to signify that it has been measured
+    gpio_hal_arch_write_pin(out_port1, out_pin1, 0);
+    gpio_hal_arch_write_pin(out_port2, out_pin2, 1);
+    ms1b = tsch_current_asn.ms1b;
+    ls4b = tsch_current_asn.ls4b;
+    // Build package to send
+    snprintf(udp_buf, sizeof(udp_buf), "msb: %d, lsb: %ld", ms1b, ls4b);
+    simple_udp_sendto(&udp_conn, udp_buf, strlen(udp_buf), &rpi_ip);
+    // Print to serial
+    LOG_INFO("Sent ASN (msb: %d, lsb: %ld) to ", ms1b, ls4b);
+    LOG_INFO_6ADDR(&rpi_ip);
+    LOG_INFO_("\n");
+    // Set pin to low again
+    gpio_hal_arch_write_pin(out_port1, out_pin1, 1);
+    gpio_hal_arch_write_pin(out_port2, out_pin2, 0);
+    
+    /* Wait for timer to expire*/
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
   }
 
   PROCESS_END();
--- sensor/sensor.c
+++ sensor/sensor.c
@@ -1,9 +1,9 @@
 #include "contiki.h"
 // For routing
-#include "net/routing/routing.h"
+#include "net/routing/routing.h" // To check if node is part of network
 // For MAC layer
-#include "net/netstack.h"
-#include "os/net/mac/tsch/tsch.h"
+#include "net/netstack.h" // Start TSCH
+#include "os/net/mac/tsch/tsch.h" // Access ASN
 // For sending UDP packets over IPv6
 #include "net/ipv6/simple-udp.h"
 #include "os/net/ipv6/uiplib.h"
@@ -12,12 +12,12 @@
 #include "dev/gpio-hal.h"
 #include "arch/platform/simplelink/cc13xx-cc26xx/launchpad/cc2650/Board.h"
 
-// Project General
+// Project general
 #include "project-conf.h"
 
-// Setup for logging to serial 
+// Setup for logging to serial
 #include "sys/log.h"
-#define LOG_MODULE "Sensor Node"
+#define LOG_MODULE "AoI Sensor Node"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 #define UDP_CLIENT_PORT	8765
@@ -27,69 +27,76 @@
 
 static struct simple_udp_connection udp_conn; // UDP Connection
 
-gpio_hal_pin_t out_pin = Board_PIN_GLED; // Pin for green LED and DIO7
+gpio_hal_pin_t out_pin1 = Board_PIN_RLED;
+gpio_hal_pin_t out_pin2 = Board_PIN_GLED;
 
-#if GPIO_HAL_PORT_PIN_NUMBERING
-#define out_port   GPIO_HAL_NULL_PORT
+#if !GPIO_HAL_PORT_PIN_NUMBERING
+extern gpio_hal_port_t out_port1, out_port2_3;
+extern gpio_hal_port_t btn_port;
+#else
+#define out_port1   GPIO_HAL_NULL_PORT
+#define out_port2 GPIO_HAL_NULL_PORT
+#define btn_port    GPIO_HAL_NULL_PORT
 #endif
 
 /*---------------------------------------------------------------------------*/
-PROCESS(udp_client_process, "UDP client");
-AUTOSTART_PROCESSES(&udp_client_process);
+PROCESS(aoi_sensor_node_process, "AoI Sensor Node");
+AUTOSTART_PROCESSES(&aoi_sensor_node_process);
 /*---------------------------------------------------------------------------*/
 static void
-udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-        {
-          LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
-          LOG_INFO_6ADDR(sender_addr);
-          LOG_INFO_("\n");
-        }
+udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr, uint16_t sender_port, 
+                const uip_ipaddr_t *receiver_addr, uint16_t receiver_port, const uint8_t *data, uint16_t datalen) {
+  LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+  LOG_INFO_("\n");
+}
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(udp_client_process, ev, data)
-{
+PROCESS_THREAD(aoi_sensor_node_process, ev, data) {
   static struct etimer periodic_timer; // Timer for sending data periodically
   static char udp_buf[32]; // char[] buffer for sending data to RPi
   static uip_ipaddr_t rpi_ip; // IP for the RPi
   uiplib_ipaddrconv("fd00:0:0:0:0:0:0:1", &rpi_ip);
+  // Int's for ASN
+  static int ms1b;
+  static long ls4b;
 
   PROCESS_BEGIN(); // Begin the process
 
   /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
-                      UDP_SERVER_PORT, udp_rx_callback);
-
-  /* Set a timer for SEND_INTERVAL seconds*/
-  etimer_set(&periodic_timer, SEND_INTERVAL);
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, udp_rx_callback);
 
   /* Enable the TSCH layer*/
   NETSTACK_MAC.on();
 
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+  LOG_INFO("AoI Sensor Node has started\n");
 
+  while(1) {
+    /* Set a timer for SEND_INTERVAL seconds*/
+    etimer_set(&periodic_timer, SEND_INTERVAL);
+    
     if(NETSTACK_ROUTING.node_is_reachable()) {
       /* Send ASN to RPi */
-      LOG_INFO("Send ASN to ");
+      // Set pin high, to signify that it has been measured
+      gpio_hal_arch_write_pin(out_port1, out_pin1, 0);
+      gpio_hal_arch_write_pin(out_port2, out_pin2, 1);
+      int ms1b = tsch_current_asn.ms1b;
+      long ls4b = tsch_current_asn.ls4b;
+      // Build package to send
+      snprintf(udp_buf, sizeof(udp_buf), "msb: %d, lsb: %ld", ms1b, ls4b);
+      simple_udp_sendto(&udp_conn, udp_buf, strlen(udp_buf), &rpi_ip);
+      // Print to serial
+      LOG_INFO("Sent ASN (msb: %d, lsb: %ld) to ", ms1b, ls4b);
       LOG_INFO_6ADDR(&rpi_ip);
       LOG_INFO_("\n");
-      snprintf(udp_buf, sizeof(udp_buf), "msb: %d, lsb: %ld", tsch_current_asn.ms1b, tsch_current_asn.ls4b);
-      gpio_hal_arch_write_pin(out_port, out_pin, 1);
-      LOG_INFO("Pulled DIO7 high\n");
-      simple_udp_sendto(&udp_conn, udp_buf, strlen(udp_buf), &rpi_ip);
-      gpio_hal_arch_write_pin(out_port, out_pin, 0);
-      LOG_INFO("Pulled DIO7 low\n");
+      // Set pin to low again
+      gpio_hal_arch_write_pin(out_port1, out_pin1, 1);
+      gpio_hal_arch_write_pin(out_port2, out_pin2, 0);
     } else {
       LOG_INFO("RPi is not reachable yet\n");
     }
 
-    /* Reset timer */
-    etimer_set(&periodic_timer, SEND_INTERVAL);
+    /* Wait for the timer to expire */
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
   }
 
   PROCESS_END();
