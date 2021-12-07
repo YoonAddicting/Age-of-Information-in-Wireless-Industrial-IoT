d86637ed98b35c7e69e20a05b7129854e4c420d1
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2021-12-07 21:30:13 +0100

    Cleanup of past experiements

--- Makefile
+++
@@ -1,9 +0,0 @@
-MAKE_MAC = MAKE_MAC_TSCH
-MODULES += os/services/shell
-
-CONTIKI_PROJECT = temp-sensor
-all: $(CONTIKI_PROJECT)
-	
-
-CONTIKI = ..
-include $(CONTIKI)/Makefile.include
--- project-conf.h
+++
@@ -1,8 +0,0 @@
-#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
-#define TSCH_LOG_CONF_PERLOT                     0
-#define NBR_TABLE_CONF_MAX_NEIGHBORS               6
