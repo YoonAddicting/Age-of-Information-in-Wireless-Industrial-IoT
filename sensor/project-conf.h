7881ea655b92851c2f186209152713e85e7ce967
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2021-12-07 21:17:18 +0100

    Proper formatting of sensor's project-conf.h

--- sensor/project-conf.h
+++ sensor/project-conf.h
@@ -1,9 +1,14 @@
-#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_WARN //LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_WARN //LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_WARN //LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_WARN //LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_WARN //LOG_LEVEL_INFO
-#define TSCH_LOG_CONF_PERLOT                     0
-#define TSCH_LOG_CONF_PER_SLOT		0
-#define NBR_TABLE_CONF_MAX_NEIGHBORS	4
-#define UIP_CONF_BUFFER_SIZE 200
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define LOG_CONF_LEVEL_IPV6                         LOG_LEVEL_WARN 
+#define LOG_CONF_LEVEL_RPL                          LOG_LEVEL_WARN 
+#define LOG_CONF_LEVEL_6LOWPAN                      LOG_LEVEL_WARN 
+#define LOG_CONF_LEVEL_TCPIP                        LOG_LEVEL_WARN 
+#define LOG_CONF_LEVEL_MAC                          LOG_LEVEL_WARN
+#define TSCH_LOG_CONF_PERLOT                        0
+#define TSCH_LOG_CONF_PER_SLOT		                0
+#define NBR_TABLE_CONF_MAX_NEIGHBORS	            4
+#define UIP_CONF_BUFFER_SIZE                        200
+
+#endif // PROJECT_CONF_H
