e53d3aeee9b9395aac00e563e1c10f958cf8f15c
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2021-12-07 21:17:56 +0100

    Cleanup of border_router's project-conf.h to remove unused functionality

--- border_router/project-conf.h
+++ border_router/project-conf.h
@@ -1,27 +1,14 @@
-#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
-//#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
-#define TSCH_LOG_CONF_PERLOT                     0
-#define TSCH_LOG_CONF_PER_SLOT		0
-#define NBR_TABLE_CONF_MAX_NEIGHBORS	4
-#define UIP_CONF_BUFFER_SIZE 200
-
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#ifndef WEBSERVER_CONF_CFS_CONNS
-#define WEBSERVER_CONF_CFS_CONNS 2
-#endif
-
-#ifndef BORDER_ROUTER_CONF_WEBSERVER
-#define BORDER_ROUTER_CONF_WEBSERVER 1
-#endif
-
-#if BORDER_ROUTER_CONF_WEBSERVER
-#define UIP_CONF_TCP 1
-#endif
+#define LOG_CONF_LEVEL_IPV6                         LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_RPL                          LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_6LOWPAN                      LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_TCPIP                        LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_MAC                          LOG_LEVEL_WARN
+#define TSCH_LOG_CONF_PERLOT                        0
+#define TSCH_LOG_CONF_PER_SLOT		                0
+#define NBR_TABLE_CONF_MAX_NEIGHBORS	            4
+#define UIP_CONF_BUFFER_SIZE                        200
 
 #endif /* PROJECT_CONF_H_ */
